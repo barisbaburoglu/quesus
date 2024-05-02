4f35115cdb775c4cf56281fb04d37be53376d626
barisbaburoglu <barisbaburoglu@gmail.com>
2024-05-02 13:02:39 +0300

    new build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dcb9807f55a7f414634876b6045e875a",
-"/": "dcb9807f55a7f414634876b6045e875a",
-"main.dart.js": "948100964c7061be03160a60c0454a2b",
+"index.html": "cd7897940fbfffe4fa20f35f48aafddf",
+"/": "cd7897940fbfffe4fa20f35f48aafddf",
+"main.dart.js": "63354681bfcead5859fd7b20edae9854",
 "manifest.json": "979ec1443651f487e8946eb3527ffb01",
 "version.json": "2747256a3cb7100681b94f3d42effe25"
 };
